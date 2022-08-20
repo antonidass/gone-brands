15bbab3ed040a58e993888c57b768786b08aa763
Антон <akrikoff@gmail.com>
2022-08-20 19:10:27 +0300

    fix api endpoint

--- client/remain-in-russia/src/context/company/CompanyActions.js
+++ client/remain-in-russia/src/context/company/CompanyActions.js
@@ -1,5 +1,8 @@
-const COMPANIES_URL = process.env.REACT_APP_COMPANIES_URL;
-const COMPANY_URL = process.env.REACT_APP_COMPANY_URL;
+// const COMPANIES_URL = process.env.REACT_APP_COMPANIES_URL;
+// const COMPANY_URL = process.env.REACT_APP_COMPANY_URL;
+
+const COMPANIES_URL = "https://remain-in-russia.herokuapp.com/api/v1";
+// const COMPANY_URL = process.env.REACT_APP_COMPANY_URL;
 
 export const getCompanies = async () => {
   const response = await fetch(COMPANIES_URL);
