c6f27f666f943f9c6ebf3fcd745cf7cece1c369f
Антон <akrikoff@gmail.com>
2022-08-20 19:13:03 +0300

    add something

--- client/remain-in-russia/src/context/company/CompanyActions.js
+++ client/remain-in-russia/src/context/company/CompanyActions.js
@@ -5,13 +5,13 @@ const COMPANIES_URL = "https://remain-in-russia.herokuapp.com/api/v1";
 // const COMPANY_URL = process.env.REACT_APP_COMPANY_URL;
 
 export const getCompanies = async () => {
-  const response = await fetch(COMPANIES_URL);
+  const response = await fetch(COMPANIES_URL, { mode: "no-cors" });
   const data = await response.json();
   return data;
 };
 
 export const getCompany = async (id) => {
-  const response = await fetch(`${COMPANIES_URL}/${id}`);
+  const response = await fetch(`${COMPANIES_URL}/${id}`, { mode: "no-cors" });
   const data = await response.json();
   return data;
 };
